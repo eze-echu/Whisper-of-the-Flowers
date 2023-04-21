fdd1e7e46b9936aa94ec47a2f879a34560c7c1a9
Luca-Elpopas <114613577+MrPopas@users.noreply.github.com>
2023-04-21 13:19:10 -0300

    Works

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -25,15 +25,37 @@ public class StoryController : MonoBehaviour
 
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            HandleStory("Decrease_Of_Love", 1, 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            HandleStory("Love", 1, 1);
+        }
+    }
 
 
     public void HandleStory(string subject, int intentValues, int formalityValues)
     {
-        if (subject == "Decrease_Of_Love" && _giveGood)//_requieredSubject[_currentStory] && _currentStory < _history.Count && _currentStory < _requieredSubject.Count)
+      
+
+
+        if (subject == "Decrease_Of_Love")//_requieredSubject[_currentStory] && _currentStory < _history.Count && _currentStory < _requieredSubject.Count)
         {
-            _currentStory ++;
-            _dialogueController.ShowSpecificQuest(_history[_currentStory]);
+            _currentStory++;
             _giveBad = true;
+            if (_giveGood && _giveBad)
+            {
+                EndMhe();
+                return;
+            }
+            _dialogueController.ShowSpecificQuest(_history[_currentStory]);
+            _currentStory++;
+            print(_giveBad);
             //IGNORAR
             /*
             if (intentValues > 0 && formalityValues > 0)
@@ -60,22 +82,29 @@ public class StoryController : MonoBehaviour
             //_dialogueController.ShowSpecificQuest(_history[_currentStory]);
             //_currentStory++;
         }
-        else if(_giveBad)
+        else 
         {
             _currentStory += 2;
-            _dialogueController.ShowSpecificQuest(_history[_currentStory]);
             _giveGood = true;
-
-            
-            Debug.Log("Mal");
-        }
-        else
-        {
-            _currentStory = 6;
+            if (_giveGood && _giveBad)
+            {
+                EndMhe();
+                return;
+            }
             _dialogueController.ShowSpecificQuest(_history[_currentStory]);
+           
+
+            print(_giveGood);
         }
+       
       
 
+    }
+
+    public void EndMhe()
+    {
+        _currentStory = 5;
+        _dialogueController.ShowSpecificQuest(_history[_currentStory]);
     }
 }
 
