7b15324f50839a523c7dab7ef204e68102b1a99f
Ezequiel Oyhambure <ezeoyhambure@gmail.com>
2023-05-21 17:05:49 -0300

    Timely HandInZone

--- Assets/Scripts/HandInZone.cs
+++ Assets/Scripts/HandInZone.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class HandInZone : MonoBehaviour, IDropZone
 {
+    private delegate void handInActions();
+    private handInActions handInAfter;
+    private handInActions handInBefore;
     public bool DropAction(GameObject a = null)
     {
         print("HandIN");
         gameObject.tag = "DropZone";
-        a?.transform.GetComponent<Bouquet>()?.SendVariableToStoryManager();
-        FindObjectOfType<FlowerHandler>()?.ResetWorkspace();
-        a?.GetComponent<Bouquet>()?.ResetToOriginalState();
+        handInBefore = delegate
+        {
+            a?.transform.GetComponent<Bouquet>()?.SendVariableToStoryManager();
+        };
+        handInAfter = delegate
+         {
+             FindObjectOfType<FlowerHandler>()?.ResetWorkspace();
+             a?.GetComponent<Bouquet>()?.ResetToOriginalState();
+         };
+        waitFewSeconds(3);
         return true;
     }
 
+    private IEnumerable waitFewSeconds(float time)
+    {
+        handInBefore();
+        yield return new WaitForSeconds(time);
+        handInAfter();
+    }
 }
