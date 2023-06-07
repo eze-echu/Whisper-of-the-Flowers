a559a2ba41bbbb4646422e97fe203a21d01841e8
Luca-Elpopas <114613577+MrPopas@users.noreply.github.com>
2023-06-07 19:38:09 -0300

    fuck you canvas

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -12,7 +12,7 @@ public class DialogueManager : MonoBehaviour
 
     public void Start()
     {
-        ShowRequest("pipo");
+        ShowRequest("pipopipo");
     }
 
 
@@ -51,25 +51,79 @@ public class DialogueManager : MonoBehaviour
 
     private void CreateTextObject(string text)
     {
+        /*
         GameObject newTextObject = Instantiate(textObjectPrefab, contentTransform);
-        TextMeshProUGUI newText = newTextObject.GetComponentInChildren<TextMeshProUGUI>();
-        newText.text = text;
+        TextMeshProUGUI textMesh = newTextObject.GetComponentInChildren<TextMeshProUGUI>();
+        RectTransform newTextRect = newTextObject.GetComponent<RectTransform>();
 
-        // Ajustar el ancho del fondo según el tamaño del texto
-        RectTransform textRect = newText.GetComponent<RectTransform>();
-        RectTransform backgroundRect = newTextObject.GetComponent<RectTransform>();
+        textMesh.text = text;
 
-        float textWidth = textRect.sizeDelta.x;
-        if (textWidth > maxTextWidth)
-        {
-            float scaleFactor = maxTextWidth / textWidth;
-            textRect.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
-        }
+        // Ajustar el ancho del objeto vacío según el tamaño del texto
+        float textWidth = textMesh.preferredWidth;
+        float objectWidth = Mathf.Min(textWidth, maxTextWidth); // Limitar el ancho del objeto al máximo permitido
+        Vector2 objectSize = newTextRect.sizeDelta;
+        objectSize.x = objectWidth;
+        newTextRect.sizeDelta = objectSize;
 
         // Ajustar el tamaño del Content Size Fitter para mostrar el nuevo texto
         LayoutRebuilder.ForceRebuildLayoutImmediate(contentTransform.GetComponent<RectTransform>());
 
         // Asegurarse de que el ScrollView se desplace hasta el final para mostrar el nuevo texto
+        ScrollRect scrollRect = GetComponent<ScrollRect>();
+        scrollRect.normalizedPosition = new Vector2(0, 0);
+
+        // Restaurar el tamaño original del texto
+        textMesh.rectTransform.sizeDelta = new Vector2(0f, textMesh.rectTransform.sizeDelta.y);
+        */
+        GameObject newTextObject = Instantiate(textObjectPrefab, contentTransform);
+        TextMeshProUGUI textMesh = newTextObject.GetComponentInChildren<TextMeshProUGUI>();
+
+        textMesh.text = text;
+
+        AdjustObjectPosition(newTextObject);
+        AdjustBackgroundWidth(newTextObject, textMesh);
+        //AdjustContentSize();
+        //ScrollToBottom();
+
+
+    }
+
+    private void AdjustBackgroundWidth(GameObject newTextObject, TextMeshProUGUI textMesh)
+    {
+        RectTransform backgroundRect = newTextObject.GetComponentInChildren<Image>().rectTransform;
+        float textWidth = textMesh.preferredWidth;
+        float backgroundWidth = Mathf.Min(textWidth, maxTextWidth);
+        Vector2 backgroundSize = backgroundRect.sizeDelta;
+        backgroundSize.x = backgroundWidth;
+        backgroundRect.sizeDelta = backgroundSize;
+    }
+
+    private void AdjustObjectPosition(GameObject newTextObject)
+    {
+        RectTransform newTextRect = newTextObject.GetComponent<RectTransform>();
+        RectTransform contentRect = contentTransform.GetComponent<RectTransform>();
+
+        newTextRect.SetParent(contentRect);
+        newTextRect.localScale = Vector3.one;
+
+        // Establecer la posición del newTextObject en el borde derecho del contentTransform
+        Vector2 anchoredPosition = newTextRect.anchoredPosition;
+        float newX = (contentRect.rect.width * 0.5f) + (newTextRect.rect.width ); // Calcular la posición X
+        float newY = (contentRect.rect.height * 0.5f) - (newTextRect.rect.height * 0.5f); // Calcular la posición Y
+        anchoredPosition.x = newX;
+        anchoredPosition.y = newY;
+        newTextRect.anchoredPosition = anchoredPosition;
+
+       
+    }
+
+    private void AdjustContentSize()
+    {
+        LayoutRebuilder.ForceRebuildLayoutImmediate(contentTransform.GetComponent<RectTransform>());
+    }
+
+    private void ScrollToBottom()
+    {
         ScrollRect scrollRect = GetComponent<ScrollRect>();
         scrollRect.normalizedPosition = new Vector2(0, 0);
     }
