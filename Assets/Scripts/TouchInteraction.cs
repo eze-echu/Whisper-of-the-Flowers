e95298ca789f7b930e9cca1a63f3bdbe7c0a067b
Luca-Elpopas <114613577+MrPopas@users.noreply.github.com>
2023-06-13 16:31:58 -0300

    esrek

---
+++ Assets/Scripts/TouchInteraction.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class TouchInteraction : MonoBehaviour
+{
+    [SerializeField] GameObject _target;
+
+   
+
+
+    private void OnMouseDown()
+    {
+        if (CheckClickedObject())
+        {
+            ToggleObjectActivation();
+        }
+    }
+
+    private bool CheckClickedObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            return (hit.collider.gameObject == gameObject);
+        }
+
+        return false;
+    }
+
+    private void ToggleObjectActivation()
+    {
+        if (_target != null)
+        {
+            _target.SetActive(!_target.activeSelf);
+        }
+    }
+}
